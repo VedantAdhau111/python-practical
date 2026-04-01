7cfa2d1636ff4075d7aae7b298087beefa1f1490
VedantAdhau111 <vedantadhau111@gmail.com>
2026-04-01 12:31:01 +0530

    Update steel.py

--- steel.py
+++ steel.py
@@ -22,3 +22,19 @@ else:
     grade = 5
 
 print("Steel Grade:", grade)
+
+# Ohm's Law Program
+
+voltage = float(input("Enter voltage (V): "))
+resistance = float(input("Enter resistance (R): "))
+
+current = voltage / resistance
+
+print("Current =", current, "A")
+
+if current < 0.5:
+    print("Low current")
+elif 0.5 <= current <= 2:
+    print("Normal current")
+else:
+    print("High current")
